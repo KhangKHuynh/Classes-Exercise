b3d24e679db4038f7258adc759fce77aefbfad70
Khang Kyle Huynh <khanghuynh@Khangs-MacBook-Pro.local>
2025-01-02 15:14:55 -0600

    Car Model Assignment

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "Toyota";
+            carOne.Model = "4Runner";
+            carOne.Year = 2019;
+            
+            Console.WriteLine($"This is a {carOne.Make} {carOne.Model}, made in the year {carOne.Year}");
         }
     }
 }
